a9e21784ddf83f21076bd8fb44d59735a5488498
Francisca <fraan928@gmail.com>
2021-11-28 07:53:39 -0300

    Changed the display of values

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,9 +1,26 @@
+import dayjs from "dayjs";
+import { Date, Description, Info, Value } from "../styles/TransactionsStyle";
+
 export default function Wallet({ wallet }) {
+    const {
+        date,
+        description,
+        value,
+    } = wallet;  
+
+    const dateFormat = dayjs(date).format('DD/MM');
+
+    let valueFormat = value;
+    if (value < 0) {
+        valueFormat = value * -1;
+    }
+    valueFormat = value.toFixed(2);
+
     return (
-        <>
-            <span>{wallet.date}aaa</span>
-            <span>{wallet.description}</span>
-            <span>{wallet.value}</span>
-        </>
+        <Info>
+            <Date>{ dateFormat }</Date>
+            <Description>{ description }</Description>
+            <Value value={value}>{ valueFormat }</Value>
+        </Info>
     );
 }
